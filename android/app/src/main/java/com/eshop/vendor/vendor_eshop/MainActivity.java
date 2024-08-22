6b12305b3481fb1858c26ee84b1708e4a83fd507
Alphawizz Flutter Team <alphawizzflutterteam@gmail.com>
2024-08-22 17:40:31 +0530

    first commit

---
+++ android/app/src/main/java/com/eshop/vendor/vendor_eshop/MainActivity.java
@@ -0,0 +1,6 @@
+package com.bpremium.seller;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/Helper/Constant.dart
@@ -0,0 +1,19 @@
+final String appName = ' B premium Seller';
+
+final String packageName = 'com.bpremium.seller';
+
+final String iosPackage = 'com.bpremium.seller';
+
+
+
+final String baseUrl = 'https://bpremium.in/seller/app/v1/api/';
+
+final String jwtKey = "78084f1698c9fcff5a668b68dcd103db39be2605";
+
+final int timeOut = 50;
+const int perPage = 10;
+
+const String BANK_DETAIL = 'Bank Details:\n' +
+    'Account No :123XXXXX\n' +
+    'IFSC Code: 123XXX \n' +
+    'Name: Abc xyz';
